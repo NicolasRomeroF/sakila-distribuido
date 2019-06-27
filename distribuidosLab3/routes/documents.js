9b0e475fcfeb38411464d96fbfe72aa40db0d489
Franco <franco.leal@usach.cl>
2019-06-26 23:31:48 -0400

    es express

---
+++ distribuidosLab3/routes/documents.js
@@ -0,0 +1,20 @@
+var express = require('express');  
+var router = express.Router();
+
+var elastic = require('../models/ElasticSearchModel');
+
+/* GET suggestions */
+router.get('/suggest/:input', function (req, res, next) {  
+  elastic.getSuggestions(req.params.input).then(function (result) { res.json(result) });
+});
+
+/* POST document to be indexed */
+router.post('/', function (req, res, next) {  
+  elastic.addDocument(req.body).then(function (result) { res.json(result) });
+});
+
+router.get('/testing/:input',function (req,res,next){
+    elastic.getMovie(req.params.input).then(function (result) {res.json(result)});
+})
+
+module.exports = router;
