e52291bfdcda8bf6cef9f0442224787bcf5a45dd
Franco <franco.leal.@usach.cl>
2019-06-30 17:10:41 -0400

    hotfix endpoint es

--- routes/documents.js
+++ routes/documents.js
@@ -13,12 +13,13 @@ router.post('/', function (req, res, next) {
   elastic.addDocument(req.body).then(function (result) { res.json(result) });
 });
 
-router.get('/:input',function (req,res,next){
-    elastic.getMovie(req.params.input).then(function (result) {res.json(result)});
-})
-
 router.get('/initIndex',function(req,res,next){
   elastic.initIndex().then(function (result) {res.json(result)})
 })
+
+router.get('/:input',function (req,res,next){
+  elastic.getMovie(req.params.input).then(function (result) {res.json(result)});
+})
+
 
 module.exports = router;
